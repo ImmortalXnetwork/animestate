6c1f5ca7e2304f317612277f1244daba22efbd8b
Mohammed suhail <119152757+ImmortalXnetwork@users.noreply.github.com>
2024-02-05 13:23:16 +0000

    Update _config.php

--- _config.php
+++ _config.php
@@ -2,7 +2,7 @@
 
 $websiteTitle = "AnimeState"; // Website Name
 $websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
-$websiteLogo = $animestate.live . "/files/images/logo.png?v=2"; // Logo
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
 $contactEmail = "fazal0207@outlook.com"; // Contact Email
 
 $version = "0.3";
